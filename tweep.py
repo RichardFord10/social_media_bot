a7c56cb6d7361a17e307c78a2634b6e3aa7282df
Richard Ford <36119878+RichardFord10@users.noreply.github.com>
2023-01-04 22:13:29 -0500

    refactoring and adding params

--- tweep.py
+++ tweep.py
@@ -3,33 +3,31 @@ import keys
 import geocoder
 
 client = tweepy.Client(bearer_token=keys.bearer, consumer_key=keys.api_key, consumer_secret=keys.api_secret, access_token=keys.access_token, access_token_secret=keys.access_token_secret, wait_on_rate_limit=True)
-
-
-
-class Tweet_Functions:
-
-    def like_tweet_from_hashtag(self, hashtag, rate_limit):
+project_url = r"https://github.com/RichardFord10/social_media_bot/blob/master/tweep.py"
+class Twitter_Functions:
+    
+    #Function to like a set number of tweets based on a single hashtag
+    #@params hashtag: the hashtag to search, rate_limit: the amount of tweets to like, print_tweets: print the liked tweets to the terminal
+    def like_tweet_from_hashtag(self, hashtag, rate_limit, print_tweets = True):
         query = '#'+hashtag+' -is:retweet lang:en'
         tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=rate_limit)
-        
         for tweet in tweets.data:
-            print(tweet.text)
             if len(tweet.context_annotations) > 0:
-                print(tweet.context_annotations)
-                print('\n')
-                print(tweet.id)
-                client.like(tweet.id)
-    
-    
-    def make_tweet(self, tweet_text, hashtags = []):
-        hashtag = ["#" + tag for tag in hashtags]
-        hashtag_string = ' '.join(hashtag)
-        full_tweet = tweet_text + ' ' + hashtag_string
+                if(not print_tweets):
+                    client.like(tweet.id)
+                    print("Tweet with ID:"+str(tweet.id)+" liked")
+                else:
+                    print(tweet.context_annotations)
+                    client.like(tweet.id)
+
+    #Function to make a tweet
+    #@params tweet_text: text of tweet, hashtag_array: an array of hashtags to include
+    def make_tweet(self, tweet_text, hashtag_array = []):
+        full_tweet = tweet_text + ' ' + ' '.join(["#" + tag for tag in hashtag_array])
         if(client.create_tweet(text=full_tweet)):
             print("Tweet "+full_tweet+" has been sent successfully!")
     
 
-
-Twitter = Tweet_Functions()
-Twitter.make_tweet("This tweet is being sent from an automated script!", ['programming', 'python', 'tweepy', 'twitterapi'])
-# Twitter.like_tweet_from_hashtag("learnprogramming", 10)
+Twitter = Twitter_Functions()
+# Twitter.like_tweet_from_hashtag("programming", 10, False)
+Twitter.make_tweet("Consider following my account if youre interested in developement related things! You can view my source code here--> "+project_url, ['programming', 'developement', 'tweepy', 'twitterapi', 'python', 'github'])
